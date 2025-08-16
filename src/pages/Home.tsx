4b88e142056308b93107441dcc8b28999bf4a2c3
Waqashadi <waqashadi075@gmail.com>
2025-08-16 16:45:13 +0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -252,7 +252,7 @@ const Home = () => {
                 your home. Our certified nurses provide personalized care to
                 help you live well every day.
               </p>
-              <div className="flex flex-col justify-between items-center sm:flex-row gap-4 w-full sm:w-full">
+              <div className="flex flex-col justify-between items-center sm:flex-row gap-4 w-full sm:w-full ">
                 <AppointmentButton
                   type="consultation"
                   size="lg"
