ca69e483bd451e6263d8f7e2d60b604673f58fe4
Waqashadi <waqashadi075@gmail.com>
2025-08-16 16:53:32 +0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -423,7 +423,7 @@ const Home = () => {
         </div>
       </section>
 
-      {/* Testimonials Section */}
+      {/* Testimonials Section   */}
       <section className="py-20">
         <div className="container mx-auto px-4">
           <div className="text-center mb-16">
