fae2989492564e74ea1a6854f2254f8a07f37b25
Waqashadi <waqashadi075@gmail.com>
2025-08-16 16:45:55 +0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="bg-medical-navy text-white">
       <div className="container mx-auto px-4 py-12">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 ">
           {/* Company Info */}
           <div className="space-y-4">
             <div className="flex items-center ml-2">
